ef2a98c41bf67b77b535dadaf9df0c792ab3aca8
Emmay17 <emmamayele123@gmail.com>
2025-02-18 19:43:34 +0100

    correction

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 // toUpperCaseFunction
 const phrase : string = "emmanuel mayele";
 
-function toUpperCaseFunction(text: string){
-    return text.toLocaleUpperCase();
+function toUpperCaseFunction(text: string) : string{
+    return text.toUpperCase();
 }
 console.log(toUpperCaseFunction(phrase));
 
